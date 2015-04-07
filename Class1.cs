927ceeb118e74c8de84345c9c2af2f30f721e4de
Brandon King <yoman650@gmail.com>
2015-04-07 09:14:25 -0700

    Init

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniSharp
+{
+    public class Class1
+    {
+    }
+}
